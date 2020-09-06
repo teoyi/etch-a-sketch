411739ab27bfef985690189cae18324432d6ec29
teoyi <yipengteo@yahoo.com.sg>
2020-09-05 18:07:05 -0700

    successfully added resize functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,45 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
+let row = 10; 
+let col = 10;
 
 // Grid creation 
 function grid(row,col) {
     container.style.setProperty('--grid-rows', row);
     container.style.setProperty('--grid-cols', col);
-    for (c=0; c < (row*col); c++) { 
+    for (c=0; c<(row*col); c++) { 
         // Adding div logic
         let box = document.createElement('div');
-        //box.innerText = (c+1);
+        box.innerText = (c+1);
         container.appendChild(box).className = "grid-item";
         //console.log(c)
     };
 };
 
 // Initial Grid Load 
-grid(10,10);
+grid(row,col);
+// Creating grid based on user input upon clicking resize
+const resize = document.querySelector(".resize")
 
-// Creating grid based on user input upon clicking resize  
-// let row = prompt("How many rows?");
-// let col = prompt("How many columns?");
-// grid(row,col);
+function reSize() { 
+    let row = prompt("How many rows?");
+    let col = prompt("How many columns?");
+    // Clears out the original dimensions 
+    document.getElementById("container").innerHTML = "";
+    // Append new dimensions to div 
+    grid(row,col);
+}
+resize.addEventListener("click", function() {
+    reSize();
+});
+
 
 
 // DOM for hover effect 
-const box = document.querySelectorAll(".grid-item");
-console.log(box.length)
+const cells = document.querySelectorAll(".grid-item");
+console.log(cells.length)
 
-box.forEach((box) => {
-    box.addEventListener("mouseenter", (e) => {
+cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", (e) => {
         e.target.style.backgroundColor = "black";
     });
 });
-//box.addEventListener("mouseover", darken, false);
-//box.addEventListener("mouseout", revert, false);
-
-function darken() {
-    box.setAttribute("style", "background-color: black;");
-}
-
-// function revert() {
-//     box.setAttribute("style", "background-color: white;");
-// }
-
-
-// for (let i = 0; i < cell.length; i++) {
-//     console.log(cell[i]);
-//     cell[i].addEventListener("mouseenter", (e) => {
-//       e.target.style.backgroundColor = "black";
-//     });
